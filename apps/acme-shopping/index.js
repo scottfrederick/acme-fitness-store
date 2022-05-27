b9f9ba40a7029032c0bcebf367a00c3fdc82919a
Scott Frederick <sfrederick@vmware.com>
2022-05-27 16:34:30 -0500

    Update shopping deployment

--- apps/acme-shopping/index.js
+++ apps/acme-shopping/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-app.listen(8080, () => {
-    console.log("Server started on port 8080");
+app.get('/readyz', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.get('/livez', (req, res) => {
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
