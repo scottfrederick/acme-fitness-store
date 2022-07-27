80ed894e11dab1864aa723f77d2a954e97a31dce
Scott Frederick <sfrederick@vmware.com>
2022-07-27 14:57:25 -0500

    Add gateway configuration

--- deploy/tap/deploy-workloads.sh
+++ deploy/tap/deploy-workloads.sh
@@ -2,16 +2,22 @@
 
 set -eou pipefail
 
+kubectl apply -f gateway.yaml
+
 kubectl apply -f acme-payment-workload.yaml
 
 #kubectl apply -f acme-order-postgres.yaml
 #kubectl apply -f acme-order-resource-claim.yaml
 #kubectl apply -f acme-order-workload.yaml
+#kubectl apply -f acme-order-gateway-route.yaml
 
 kubectl apply -f acme-catalog-postgres.yaml
 kubectl apply -f acme-catalog-resource-claim.yaml
+kubectl apply -f acme-catalog-gateway-route.yaml
 kubectl apply -f acme-catalog-workload.yaml
 
+kubectl apply -f acme-cart-gateway-route.yaml
 kubectl apply -f acme-cart-workload.yaml
 
 kubectl apply -f acme-shopping-workload.yaml
+kubectl apply -f acme-shopping-gateway-route.yaml
---
+++ deploy/tap/remove-workloads.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eou pipefail
+
+kubectl delete -f acme-payment-workload.yaml
+
+#kubectl delete -f acme-order-postgres.yaml
+#kubectl delete -f acme-order-resource-claim.yaml
+#kubectl delete -f acme-order-workload.yaml
+#kubectl delete -f acme-order-gateway-route.yaml
+
+kubectl delete -f acme-catalog-postgres.yaml
+kubectl delete -f acme-catalog-resource-claim.yaml
+kubectl delete -f acme-catalog-gateway-route.yaml
+kubectl delete -f acme-catalog-workload.yaml
+
+kubectl delete -f acme-cart-gateway-route.yaml
+kubectl delete -f acme-cart-workload.yaml
+
+kubectl delete -f acme-shopping-workload.yaml
+kubectl delete -f acme-shopping-gateway-route.yaml
